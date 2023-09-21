a19ec861ec5773cf2ce9d9ea349da71b32ba28b7
defektive <defektive@users.noreply.github.com>
2023-09-21 14:16:21 -0600

    fix some things

--- cmd/groxi-relay/main.go
+++ cmd/groxi-relay/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+
 	"github.com/defektive/groxi/internal"
 	"github.com/defektive/groxi/pkg/relay"
 )
 
-var maxFailedConnections = flag.Int("f", 30, "The number of connections to try before giving up. NOTE: Exponential fall off")
+var maxFailedConnections = flag.Int("f", 137, "The number of connections to try before giving up. NOTE: Exponential fall off. 137 is about 24 hours. millis(numFailedConnections * numFailedConnections * 100)")
 var tunnelAddr = flag.String("t", "127.0.0.1:8081", "Address to connect to server on.")
 var version = flag.Bool("v", false, "Print groxi version")
 
